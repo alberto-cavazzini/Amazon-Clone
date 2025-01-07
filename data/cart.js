475155f149871fbdc81fa2268c52dcbef8b36812
Alexia <alexia.nottoli01@gmail.com>
2025-01-07 19:55:37 +0100

    Move addToCart into cart.js

--- data/cart.js
+++ data/cart.js
@@ -1,1 +1,36 @@
 export const cart = [];
+
+export function addToCart(productId) {
+    let matchingItem;
+
+          cart.forEach((cartItem) => {
+            if (productId === casrtItem.productId) {
+               matchingItem = cartItem; 
+            }
+            });
+
+            const quantitySelector = document.querySelector(`.js-quantity-selector-${productId}`);
+            const quantity = Number(quantitySelector.value);
+
+            const added = document.querySelector(`.js-added-to-cart-${productId}`);
+            added.classList.add('added-to-cart-visible');
+            
+            const previousTimeoutId = addedTimeouts[productId];
+            if (previousTimeoutId) {
+              clearTimeout(previousTimeoutId);
+            }
+            const timeoutId = setTimeout(() => {
+              added.classList.remove('added-to-cart-visible');
+            }, 2000);
+
+            addedTimeouts[productId] = timeoutId;
+
+            if(matchingItem) {
+              matchingItem.quantity += quantity;
+            } else {
+              cart.push({
+                productId,
+                quantity
+               });
+            }
+  }
